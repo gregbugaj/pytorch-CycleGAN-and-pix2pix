d194c2419925f2c27cf93d9dbcc3fb8b404ee829
greg <gregbugaj@yahoo.com>
2021-05-27 01:13:20 -0500

    WIP

---
+++ datasets/prepare_patches_dataset.py
@@ -0,0 +1,84 @@
+import os
+import glob
+from PIL import Image, ImageOps
+
+help_msg = """
+The processed images will be placed at --output_dir.
+Example usage:
+
+python prepare_patches_dataset.py --input_dir ./datasets/patches/train --output_dir ./datasets/patches/ready
+
+python ./prepare_patches_dataset.py  --input_dir ../datasets/patches --output_dir ../datasets/patches/ready
+
+"""
+
+def load_resized_img(path):
+    return Image.open(path).convert('RGB').resize((256, 256))
+
+def process(input_dir, output_dir, phase):
+    save_phase = 'test' if phase == 'test' else 'train'
+
+    savedir = os.path.join(output_dir, save_phase)
+    os.makedirs(savedir, exist_ok=True)
+    os.makedirs(savedir + 'A', exist_ok=True)
+    os.makedirs(savedir + 'B', exist_ok=True)
+
+    print("Directory structure prepared at %s" % output_dir)
+    
+    segmap_expr = os.path.join(input_dir, phase) + "/mask/*.png"
+    segmap_paths = glob.glob(segmap_expr)
+    segmap_paths = sorted(segmap_paths)
+
+    photo_expr = os.path.join(input_dir, phase) + "/image/*.png"
+    photo_paths = glob.glob(photo_expr)
+    photo_paths = sorted(photo_paths)
+
+    assert len(segmap_paths) == len(photo_paths), \
+        "%d images that match [%s], and %d images that match [%s]. Aborting." % (len(segmap_paths), segmap_expr, len(photo_paths), photo_expr)
+
+    for i, (segmap_path, photo_path) in enumerate(zip(segmap_paths, photo_paths)):
+        
+        segmap = load_resized_img(segmap_path)
+        segmap = ImageOps.invert(segmap)
+        photo = load_resized_img(photo_path)
+
+        # data for pix2pix where the two images are placed side-by-side
+        sidebyside = Image.new('RGB', (512, 256))
+        sidebyside.paste(segmap, (256, 0))
+        sidebyside.paste(photo, (0, 0))
+        savepath = os.path.join(savedir, "%d.jpg" % i)
+        sidebyside.save(savepath, format='JPEG', subsampling=0, quality=100)
+
+        # data for cyclegan where the two images are stored at two distinct directories
+        savepath = os.path.join(savedir + 'A', "%d_A.jpg" % i)
+        photo.save(savepath, format='JPEG', subsampling=0, quality=100)
+        savepath = os.path.join(savedir + 'B', "%d_B.jpg" % i)
+        segmap.save(savepath, format='JPEG', subsampling=0, quality=100)
+        
+        if i % (len(segmap_paths) // 10) == 0:
+            print("%d / %d: last image saved at %s, " % (i, len(segmap_paths), savepath))
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, required=True,
+                        help='Path to the input directory.')
+    parser.add_argument('--output_dir', type=str, required=True,
+                        default='./datasets/patches/ready',
+                        help='Directory the output images will be written to.')
+    opt = parser.parse_args()
+
+    print(help_msg)
+    
+    print('Preparing Dataset for val phase')
+    process(opt.input_dir, opt.output_dir, "train")
+
+    print('Preparing Dataset for val phase')
+    process(opt.input_dir, opt.output_dir, "test")
+
+    print('Done')
+
+    
+

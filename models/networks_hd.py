3caa54b879542e78ce57b07758727b89f2c93522
Greg <gregbugaj@yahoo.com>
2021-11-05 01:55:01 -0500

    Fix issue with spectral norm

--- models/networks_hd.py
+++ models/networks_hd.py
@@ -85,7 +85,7 @@ class GlobalGenerator(nn.Module):
             mult = 2**(n_downsampling - i)
             model += [nn.utils.spectral_norm(nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1)),
                        norm_layer(int(ngf * mult / 2)), activation]
-        model += [nn.ReflectionPad2d(3), nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0), nn.Tanh()]        
+        model += [nn.ReflectionPad2d(3), nn.utils.spectral_norm(nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)), nn.Tanh()]        
         self.model = nn.Sequential(*model)
             
     def forward(self, input):
